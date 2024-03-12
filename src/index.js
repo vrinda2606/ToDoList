c730653fb2a026885f4d672e7167d39daabfcec2
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-03-12 20:28:08 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
 
 import './toDoListMUI.css';
 import ToDoListMUI from './ToDoListMUI';
+// import App from './App';
 
 ReactDOM.render(<ToDoListMUI></ToDoListMUI>,document.getElementById('root'));
 // ReactDOM.render(<App />, document.getElementById('root'));
